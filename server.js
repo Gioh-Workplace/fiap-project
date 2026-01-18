d5423305e7052791da53059a64a7cdf5f9991678
Giovani Justo <giovanijusto54@gmail.com>
2026-01-18 16:10:58 -0300

    Alterações nas dependências -add DotEnv,MongoDB e Mongoose

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-import http from "http";
+import "dotenv/config";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/" : "Home page",
-    "/livros": "Livros",
-    "/autores": "Autores"
-};
-
-const server = http.createServer((req,res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT,() => {
-    console.log(`Servidor aberto na porta ${PORT}`);
+app.listen(PORT,() => {
+    console.log(`Servidor aberto`);
+    
 });
