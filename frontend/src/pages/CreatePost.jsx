c42ae7c53f95e8c627029b53587805a0624f64a2
Giovani Justo <giovanijusto54@gmail.com>
2026-03-18 21:12:41 -0300

    feat(create-post): integração do formulário com API de criação, com validação no front para a role do usuário

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,3 +1,60 @@
-export default function Post() {
-    return <h1>Create Post</h1>
+import { useState } from "react"
+import { createPost } from "../api/posts"
+
+
+export default function CreatePost() {
+  const [titulo, setTitulo] = useState("")
+  const [descricao, setDescricao] = useState("")
+
+  const role = localStorage.getItem("role")
+
+  if (role !== "professor") {
+    return <h1>Acesso negado</h1>
   }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      await createPost({
+        titulo,
+        descricao,
+        status: "rascunho"
+      })
+
+      alert("Post criado com sucesso!")
+
+      setTitulo("")
+      setDescricao("")
+    } catch (error) {
+      console.error("Erro ao criar post:", error)
+      alert("Erro ao criar post")
+    }
+  }
+ 
+  return (
+    <div>
+      <h1>Criar Post</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Título:</label>
+          <input
+            type="text"
+            value={titulo}
+            onChange={(e) => setTitulo(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Descrição:</label>
+          <textarea
+            value={descricao}
+            onChange={(e) => setDescricao(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">Criar</button>
+      </form>
+    </div>
+  )
+}
