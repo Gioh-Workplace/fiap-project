9742d676133e71717790b2181a30968da8daa232
Giovani Justo <giovanijusto54@gmail.com>
2026-03-23 21:40:50 -0300

    fix: corrige mock do postController para export default compatível com as rotas

--- backend/src/tests/post.test.js
+++ backend/src/tests/post.test.js
@@ -1,20 +1,123 @@
 import { jest } from "@jest/globals";
 import request from "supertest";
 
-await jest.unstable_mockModule(
-  "../middlewares/authMiddleware.js",
-  () => import("./__mocks__/authMiddleware.mock.js")
-);
+await jest.unstable_mockModule("../middlewares/authMiddleware.js", () => ({
+  auth: (req, res, next) => {
+    req.user = {
+      id: "mock-user-id",
+      role: req.headers["x-user-role"] || "aluno",
+    };
+    next();
+  },
+}));
 
-await jest.unstable_mockModule(
-  "../middlewares/authorizeRole.js",
-  () => import("./__mocks__/authorizeRole.mock.js")
-);
+await jest.unstable_mockModule("../middlewares/authorizeRole.js", () => ({
+  default: () => (req, res, next) => next(),
+}));
 
-await jest.unstable_mockModule(
-  "../controllers/postController.js",
-  () => import("./__mocks__/postController.mock.js")
-);
+const listarPosts = async (req, res) => {
+  return res.status(200).json({
+    total: 1,
+    data: [
+      {
+        _id: "1",
+        titulo: "Post publicado",
+        descricao: "Teste",
+        status: "publicado",
+      },
+    ],
+  });
+};
+
+const buscarPosts = async (req, res) => {
+  return res.status(200).json({
+    total: 1,
+    data: [],
+  });
+};
+
+const listarPostPorID = async (req, res) => {
+  return res.status(200).json({
+    _id: "1",
+    titulo: "Post publicado",
+    descricao: "Teste",
+    status: "publicado",
+  });
+};
+
+const cadastrarPost = async (req, res) => {
+  return res.status(201).json({
+    message: "Post criado com sucesso",
+  });
+};
+
+const atualizarPost = async (req, res) => {
+  return res.status(200).json({
+    message: "Post atualizado com sucesso",
+  });
+};
+
+const excluirPost = async (req, res) => {
+  return res.status(200).json({
+    message: "Post excluído com sucesso",
+  });
+};
+
+await jest.unstable_mockModule("../controllers/postController.js", () => {
+  const controller = {
+    listarPosts: async (req, res) => {
+      return res.status(200).json({
+        total: 1,
+        data: [
+          {
+            _id: "1",
+            titulo: "Post publicado",
+            descricao: "Teste",
+            status: "publicado",
+          },
+        ],
+      });
+    },
+
+    buscarPosts: async (req, res) => {
+      return res.status(200).json({
+        total: 1,
+        data: [],
+      });
+    },
+
+    listarPostPorID: async (req, res) => {
+      return res.status(200).json({
+        _id: "1",
+        titulo: "Post publicado",
+        descricao: "Teste",
+        status: "publicado",
+      });
+    },
+
+    cadastrarPost: async (req, res) => {
+      return res.status(201).json({
+        message: "Post criado com sucesso",
+      });
+    },
+
+    atualizarPost: async (req, res) => {
+      return res.status(200).json({
+        message: "Post atualizado com sucesso",
+      });
+    },
+
+    deletarPost: async (req, res) => {
+      return res.status(200).json({
+        message: "Post excluído com sucesso",
+      });
+    },
+  };
+
+  return {
+    default: controller,
+  };
+});
 
 const { default: app } = await import("../app.js");
 
